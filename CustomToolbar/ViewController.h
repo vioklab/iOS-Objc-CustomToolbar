88ffd9febbc1b078cafec57bdae4b7d3297222fa
bbluue IMac <bbluueImac@imac-de-bbluue.local>
2016-02-16 16:09:08 -0500

    Adding Current Location

--- CustomToolbar/ViewController.h
+++ CustomToolbar/ViewController.h
@@ -7,9 +7,14 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <MapKit/MapKit.h>
 
-@interface ViewController : UIViewController
-
-
+@interface ViewController : UIViewController <MKMapViewDelegate, CLLocationManagerDelegate> {
+    
+}
+    @property(strong, nonatomic) UIToolbar *toolbar;
+    @property(strong, nonatomic) UIView *infoPanel;
+    @property(strong, nonatomic) MKMapView *mapView;
+   
 @end
 
--- CustomToolbar/ViewController.m
+++ CustomToolbar/ViewController.m
@@ -9,7 +9,6 @@
 #import "ViewController.h"
 
 @interface ViewController (){
-    UIToolbar *toolbar;
     UIImage* toolbarImage;
     
     UIImage* barInfoButtonImage;
@@ -28,20 +27,50 @@
     UIButton *barDeleteButton;
     UIBarButtonItem *barDeleteButtonItem;
     
-    UIView *infoPanel;
-    
+    CLLocationManager *locationManager;
 }
 @end
 
 @implementation ViewController
 
+@synthesize toolbar, infoPanel, mapView;
+
+float toolBarHeight = 57.5;
+float infoPanelHeight = 350.0;
+int mapHeight = 265;
+int mapSpan = 0;
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
+    // SET TOOLBAR
+    [self set_imageToolbar];
+    
+    // SET ICONS SUB NAV
+    [self set_infoDisplay];
+    
+    // MAP VIEW
+    mapView.delegate = self;
+    mapView.showsUserLocation = YES;
+    mapView.showsBuildings = YES;
+    
+    locationManager = [CLLocationManager new];
+    if ([locationManager respondsToSelector:@selector(requestWhenInUseAuthorization)]){
+        [locationManager requestWhenInUseAuthorization];
+       
+    }
+    [locationManager startUpdatingLocation];
+    
+    self.mapView = [[MKMapView alloc] initWithFrame:CGRectMake(mapSpan, self.infoPanel.bounds.size.height-mapHeight, self.infoPanel.bounds.size.width-(mapSpan*2), mapHeight-mapSpan)];
+    [infoPanel addSubview:self.mapView];
+    
+    
+}
+
+- (void)set_imageToolbar{
     // TOOLBAR
-    toolbar = [[UIToolbar alloc] initWithFrame:CGRectMake(0, self.view.frame.size.height - 57.5, self.view.frame.size.width, 57.5)];
+    toolbar = [[UIToolbar alloc] initWithFrame:CGRectMake(0, self.view.frame.size.height - toolBarHeight, self.view.frame.size.width, toolBarHeight)];
     toolbarImage = [UIImage imageNamed: @"barra"];
     [toolbar setBackgroundImage:toolbarImage forToolbarPosition:UIToolbarPositionAny barMetrics:UIBarMetricsDefault];
     toolbar.opaque = YES;
@@ -54,7 +83,7 @@
     barInfoButton = [[UIButton alloc] initWithFrame:frameimg];
     [barInfoButton setBackgroundImage:barInfoButtonImage forState:UIControlStateNormal];
     [barInfoButton addTarget:self action:@selector(infoButtonPressed:)
-         forControlEvents:UIControlEventTouchUpInside];
+            forControlEvents:UIControlEventTouchUpInside];
     [barInfoButton setShowsTouchWhenHighlighted:NO];
     barInfoButtonItem =[[UIBarButtonItem alloc] initWithCustomView:barInfoButton];
     self.navigationItem.rightBarButtonItem=barInfoButtonItem;
@@ -65,13 +94,13 @@
     barAudioButton = [[UIButton alloc] initWithFrame:frameimg];
     [barAudioButton setBackgroundImage:barAudioButtonImage forState:UIControlStateNormal];
     [barAudioButton addTarget:self action:@selector(audioButtonPressed:)
-         forControlEvents:UIControlEventTouchUpInside];
+             forControlEvents:UIControlEventTouchUpInside];
     [barAudioButton setShowsTouchWhenHighlighted:YES];
     
     barAudioButtonItem =[[UIBarButtonItem alloc] initWithCustomView:barAudioButton];
     self.navigationItem.rightBarButtonItem=barAudioButtonItem;
     
-
+    
     barShareButtonImage = [UIImage imageNamed: @"icons_takephoto-toolbar-share"];
     frameimg = CGRectMake(0, 0, barShareButtonImage.size.width, barShareButtonImage.size.height);
     barShareButton = [[UIButton alloc] initWithFrame:frameimg];
@@ -88,7 +117,7 @@
     barDeleteButton = [[UIButton alloc] initWithFrame:frameimg];
     [barDeleteButton setBackgroundImage:barDeleteButtonImage forState:UIControlStateNormal];
     [barDeleteButton addTarget:self action:@selector(deleteButtonPressed:)
-             forControlEvents:UIControlEventTouchUpInside];
+              forControlEvents:UIControlEventTouchUpInside];
     [barDeleteButton setShowsTouchWhenHighlighted:YES];
     
     barDeleteButtonItem =[[UIBarButtonItem alloc] initWithCustomView:barDeleteButton];
@@ -104,51 +133,66 @@
     [self.view addSubview:toolbar];
     // -????
     [toolbar release];
-    
-    
+
+}
+
+
+
+- (void)set_infoDisplay{
     // SUB NAV BAR
-    infoPanel = [[UIView alloc] initWithFrame:CGRectMake(0, self.view.frame.size.height - 257.5, self.view.frame.size.width, 200)];
-      infoPanel.backgroundColor = [UIColor colorWithRed:255.0/255.0 green:255.0/255.0 blue:255.0/255.0 alpha:0.6];
+    infoPanel = [[UIView alloc] initWithFrame:CGRectMake(0, self.view.frame.size.height - (infoPanelHeight+toolBarHeight), self.view.frame.size.width, infoPanelHeight)];
+    infoPanel.backgroundColor = [UIColor colorWithRed:255.0/255.0 green:255.0/255.0 blue:255.0/255.0 alpha:1];
     [self.view addSubview:infoPanel];
     
     
     infoPanel.alpha = 0;
-    infoPanel.center = CGPointMake(infoPanel.center.x, infoPanel.center.y+100);
+    infoPanel.center = CGPointMake(infoPanel.center.x, infoPanel.center.y+(infoPanelHeight/2));
     infoPanel.transform = CGAffineTransformMakeScale(1.0, 0.001);
     
-    
     // ICONS SUB NAV
+    UIFont *subNavFont = [UIFont fontWithName:@"Helvetica" size:12.0];
+    UIColor *subNavFontColor = [UIColor colorWithRed:0.0/255.0 green:86.0/255.0 blue:120.0/255.0 alpha:1.0];
     
     
     UIImage *imgIco1 = [UIImage imageNamed:@"icons_takephoto-info-date"];
     UIImageView *icoView1 = [[UIImageView alloc] initWithImage:imgIco1];
-    icoView1.center = CGPointMake(26, 26);
-    UILabel *fromLabel = [[UILabel alloc]initWithFrame:CGRectMake(36, 26, 100, 50)];
-    fromLabel.text = @"Sunday, March 29th";
+    icoView1.center = CGPointMake(26, 22);
+    UILabel *icoLabel1 = [[UILabel alloc]initWithFrame:CGRectMake(45, 13, 300, 20)];
+    icoLabel1.text = @"Wednesday, December 29th, 2015 - 11:00 AM";
+    icoLabel1.textColor = subNavFontColor;
+    icoLabel1.font = subNavFont;
     
     
-    UIImage *imgIco2 = [UIImage imageNamed:@"icons_takephoto-info-time"];
+    UIImage *imgIco2 = [UIImage imageNamed:@"icons_takephoto-info-cloud"];
     UIImageView *icoView2 = [[UIImageView alloc] initWithImage:imgIco2];
-    icoView2.center = CGPointMake(26, 71);
+    icoView2.center = CGPointMake(26, 55);
+    UILabel *icoLabel2 = [[UILabel alloc]initWithFrame:CGRectMake(45, 46, 200, 20)];
+    icoLabel2.text = @"Synchronized";
+    icoLabel2.textColor = subNavFontColor;
+    icoLabel2.font = subNavFont;
+    
     
     UIImage *imgIco3 = [UIImage imageNamed:@"icons_takephoto-info-mb"];
     UIImageView *icoView3 = [[UIImageView alloc] initWithImage:imgIco3];
-    icoView3.center = CGPointMake(26, 116);
-    
-    UIImage *imgIco4 = [UIImage imageNamed:@"icons_takephoto-info-cloud"];
-    UIImageView *icoView4 = [[UIImageView alloc] initWithImage:imgIco4];
-    icoView4.center = CGPointMake(26, 161);
-    
-    UIImage *imgIco5 = [UIImage imageNamed:@"icons_takephoto-info-location"];
-    UIImageView *icoView5 = [[UIImageView alloc] initWithImage:imgIco5];
-    icoView5.center = CGPointMake(200, 26);
+    icoView3.center = CGPointMake(160, 54);
+    UILabel *icoLabel3 = [[UILabel alloc]initWithFrame:CGRectMake(180, 46, 200, 20)];
+    icoLabel3.text = @"20.5 Mb";
+    icoLabel3.textColor = subNavFontColor;
+    icoLabel3.font = subNavFont;
     
     NSArray *iconSubArray;
-    iconSubArray = [NSArray arrayWithObjects: icoView1, icoView2, icoView3, icoView4, icoView5, nil];
+    iconSubArray = [NSArray arrayWithObjects: icoView1, icoView2, icoView3,  nil];
     for (int i = 0; i < [iconSubArray count]; i++) {
         [infoPanel addSubview:iconSubArray[i]];
     }
     
+    NSArray *LabelsIconsSubArray;
+    LabelsIconsSubArray = [NSArray arrayWithObjects: icoLabel1, icoLabel2, icoLabel3,  nil];
+    for (int i = 0; i < [LabelsIconsSubArray count]; i++) {
+        //[LabelsIconsSubArray[i] fontAttributes:]
+        [infoPanel addSubview:LabelsIconsSubArray[i]];
+    }
+
 }
 
 - (void)infoButtonPressed:(id)sender{
@@ -159,7 +203,7 @@
         [btn setBackgroundImage:[UIImage imageNamed:@"icons_takephoto-toolbar-info"] forState:UIControlStateNormal];
         [UIView animateWithDuration:0.2 animations:^{
             infoPanel.alpha = 0;
-            infoPanel.center = CGPointMake(infoPanel.center.x, infoPanel.center.y+100);
+            infoPanel.center = CGPointMake(infoPanel.center.x, infoPanel.center.y+(infoPanelHeight/2));
             infoPanel.transform = CGAffineTransformMakeScale(1.0, 0.001);
             
         } completion:nil];
@@ -177,7 +221,7 @@
         // animation
         [UIView animateWithDuration:0.2 animations:^{
             infoPanel.alpha = 1;
-            infoPanel.center = CGPointMake(infoPanel.center.x, infoPanel.center.y-100);
+            infoPanel.center = CGPointMake(infoPanel.center.x, infoPanel.center.y-(infoPanelHeight/2));
             infoPanel.transform = CGAffineTransformMakeScale(1.0, 1.0);
             
         } completion:nil];
